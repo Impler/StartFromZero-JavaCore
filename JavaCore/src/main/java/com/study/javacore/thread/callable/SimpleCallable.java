42a1115bc72e8e4a1a89894876e6ef913b2ea83d
Impler <704090860@qq.com>
2016-02-18 17:28:05 +0800

    callable

--- JavaCore/src/main/java/com/study/javacore/thread/callable/SimpleCallable.java
+++ JavaCore/src/main/java/com/study/javacore/thread/callable/SimpleCallable.java
@@ -2,7 +2,11 @@ package com.study.javacore.thread.callable;
 
 import java.util.Random;
 import java.util.concurrent.Callable;
-
+/**
+ * 创建线程的两种方式：继承Thread和实现Runnable接口，但这两种方式都无法获取线程执行结果。
+ * Callable的出现弥补了上述的缺陷，可以在任务执行完毕后得到执行结果。
+ * @author Impler
+ */
 public class SimpleCallable implements Callable<Integer> {
 
 	@Override
