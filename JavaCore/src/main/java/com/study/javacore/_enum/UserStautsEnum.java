8a9ec66c1e4eea5895c628f02b40dee2f1b833b5
Impler <704090860@qq.com>
2016-04-16 15:29:27 +0800

    java core enum

--- JavaCore/src/main/java/com/study/javacore/_enum/UserStautsEnum.java
+++ JavaCore/src/main/java/com/study/javacore/_enum/UserStautsEnum.java
@@ -1,5 +1,14 @@
 package com.study.javacore._enum;
 
+/**
+ * java.lang.Enum<E>是所有枚举类型的父类，拥有两个私有属性ordinal和name
+ * ordinal为枚举常量的顺序号，从0开始，比如ACTIVE的ordinal为1；
+ * name为枚举常量的名称，如ACTIVE的name为ACTIVE;
+ * 
+ * 枚举类型默认继承java.lang.Enum，可以实现接口，但不能继承其他类
+ * @author Impler
+ * @date 2016年4月16日
+ */
 public enum UserStautsEnum {
 
 	INACTIVE(0, "inactive"),
